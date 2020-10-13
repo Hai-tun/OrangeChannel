20752a3dc16b0051b0dd27e795b3dded77e0a60c
Hai-tun <1346653001@qq.com>
2020-10-13 21:49:21 +0800

    Add a space

--- src/main/kotlin/pl/edu/moe/orangechannel/Config.kt
+++ src/main/kotlin/pl/edu/moe/orangechannel/Config.kt
@@ -6,7 +6,8 @@ import net.mamoe.yamlkt.Comment
 @Serializable
 data class Config(val bot : Bot, val owner : Owner) {
     @Serializable
-    data class Bot(@Comment("bot 账号") val account : Long, @Comment("bot 密码") val password : String)
+    data class Bot(@Comment("bot 账号") val account: Long, @Comment("bot 密码") val password: String)
+
     @Serializable
-    data class Owner(@Comment("主人账号")val account : Long)
+    data class Owner(@Comment("主人账号") val account: Long)
 }
